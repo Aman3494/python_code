b85ec6d8a729fa2e6532e128d4e668ebeea1d7ca
Aman3494 <amankhan4298@gmail.com>
2025-03-24 14:48:52 +0530

    less than 5 and greater than 5

---
+++ practice.py
@@ -0,0 +1,56 @@
+listdata=[1,2,3,4,5,6,7,8,9,10] 
+smalllist=[]
+largelist=[]
+
+if(listdata[0]<5):
+    smalllist.append(listdata[0])
+else:
+    largelist.append(listdata[0])
+
+if(listdata[1]<5):
+    smalllist.append(listdata[1])
+else:
+    largelist.append(listdata[1])
+
+if(listdata[2]<5):
+    smalllist.append(listdata[2])
+else:
+    largelist.append(listdata[2])
+
+if(listdata[3]<5):
+    smalllist.append(listdata[3])
+else:
+    largelist.append(listdata[3])
+
+if(listdata[4]<5):
+    smalllist.append(listdata[4])
+else:
+    largelist.append(listdata[4])
+
+if(listdata[5]<5):
+    smalllist.append(listdata[5])
+else:
+    largelist.append(listdata[5])
+
+if(listdata[6]<5):
+    smalllist.append(listdata[6])
+else:
+    largelist.append(listdata[6])
+
+if(listdata[7]<5):
+    smalllist.append(listdata[7])
+else:
+    largelist.append(listdata[7])
+
+if(listdata[8]<5):
+    smalllist.append(listdata[8])
+else:
+    largelist.append(listdata[8])
+
+if(listdata[9]<5):
+    smalllist.append(listdata[9])
+else:
+    largelist.append(listdata[9])
+
+print("numbers less than 5:", smalllist)
+print("numbers 5 or greater:", largelist)
